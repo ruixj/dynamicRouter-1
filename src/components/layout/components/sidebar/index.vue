532448cae47d742bd31f9a0ea59739e010ecd5c4
ruixj <ustb_ruixj@aliyun.com>
2018-11-03 12:46:07 +0800

    test

---
+++ src/components/layout/components/sidebar/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <el-menu mode="vertical" unique-opened :default-active="$route.path" background-color="#304156" text-color="#fff" active-text-color="#409EFF">
+      <sidebar-item :routes="routes"></sidebar-item>
+    </el-menu>
+</template>
+
+<script>
+import sidebarItem from './sidebarItem'
+import { mapGetters, mapState} from 'vuex'
+
+export default {
+  components: { sidebarItem },
+  computed: {
+    ...mapState({
+      routes: state =>  state.pagestate["approutes"] || []
+    }),
+  }
+  
+ 
+}
+</script>
