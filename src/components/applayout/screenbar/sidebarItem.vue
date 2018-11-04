f0341ae6b4edfe2f91eb3875f868a675c8d7fb33
ruixj <ustb_ruixj@aliyun.com>
2018-11-04 19:04:53 +0800

    test

---
+++ src/components/applayout/screenbar/sidebarItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="menu-wrapper">
+    <template v-for="item in routes">
+      <router-link  :to="item.path" :key="item.name">
+        <el-menu-item :index="item.path"  >
+          <span >{{item.name}}</span>
+        </el-menu-item>
+      </router-link>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'sidebarItem',
+  props: {
+    routes: {
+      type: Array
+    },
+    isNest: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+<style>
+</style
